c6aaacc9748240bf6ad15e687f5ff3293de3d47f
Giorf81 <rejroberts7@gmail.com>
2024-11-11 23:50:25 +0300

    Добавьте файлы проекта.

---
+++ 7-5-5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.NetworkInformation;
+
+namespace _7_5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Obj obj = new Obj();
+            Console.Write(Obj.Parent);
+        }
+    }
+    class Obj
+    {
+        public string Name;
+        public string Description;
+
+        public static string Parent;
+        public static int DaysInWeek;
+        public static int MaxValue;
+        static Obj() 
+        {
+            Parent = "System.Object";
+            DaysInWeek = 7;
+            MaxValue = 2000;
+        }
+    }
+}
